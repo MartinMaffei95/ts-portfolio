938542d9271b34cfdd3157bd4488d520dff851fe
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2023-04-12 11:41:43 -0300

    Fixed error in routing from header

---
+++ public/projectImg/decoLanding/Vite + React + TS_files/main.tsx
@@ -0,0 +1,18 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=37af36a5"; const _jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=90a59dc3"; const React = __vite__cjsImport1_react.__esModule ? __vite__cjsImport1_react.default : __vite__cjsImport1_react;
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=fb0232f5"; const ReactDOM = __vite__cjsImport2_reactDom_client.__esModule ? __vite__cjsImport2_reactDom_client.default : __vite__cjsImport2_reactDom_client;
+import App from '/src/App.tsx?t=1678723024810';
+import '/src/index.css?t=1678723024810';
+ReactDOM.createRoot(document.getElementById('root')).render(/*#__PURE__*/ _jsxDEV(React.StrictMode, {
+    children: /*#__PURE__*/ _jsxDEV(App, {}, void 0, false, {
+        fileName: "G:/WEBS/sdk/dashboard/chat-92-dashboard-demo/src/main.tsx",
+        lineNumber: 8,
+        columnNumber: 5
+    }, this)
+}, void 0, false, {
+    fileName: "G:/WEBS/sdk/dashboard/chat-92-dashboard-demo/src/main.tsx",
+    lineNumber: 7,
+    columnNumber: 3
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIkc6L1dFQlMvc2RrL2Rhc2hib2FyZC9jaGF0LTkyLWRhc2hib2FyZC1kZW1vL3NyYy9tYWluLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnXHJcbmltcG9ydCBSZWFjdERPTSBmcm9tICdyZWFjdC1kb20vY2xpZW50J1xyXG5pbXBvcnQgQXBwIGZyb20gJy4vQXBwJ1xyXG5pbXBvcnQgJy4vaW5kZXguY3NzJ1xyXG5cclxuUmVhY3RET00uY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpIGFzIEhUTUxFbGVtZW50KS5yZW5kZXIoXHJcbiAgPFJlYWN0LlN0cmljdE1vZGU+XHJcbiAgICA8QXBwIC8+XHJcbiAgPC9SZWFjdC5TdHJpY3RNb2RlPixcclxuKVxyXG4iXSwibmFtZXMiOlsiUmVhY3QiLCJSZWFjdERPTSIsIkFwcCIsImNyZWF0ZVJvb3QiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwicmVuZGVyIiwiU3RyaWN0TW9kZSJdLCJtYXBwaW5ncyI6IjtBQUFBLE9BQU9BLFdBQVcsUUFBTztBQUN6QixPQUFPQyxjQUFjLG1CQUFrQjtBQUN2QyxPQUFPQyxTQUFTLFFBQU87QUFDdkIsT0FBTyxjQUFhO0FBRXBCRCxTQUFTRSxVQUFVLENBQUNDLFNBQVNDLGNBQWMsQ0FBQyxTQUF3QkMsTUFBTSxlQUN4RSxRQUFDTixNQUFNTyxVQUFVO2NBQ2YsY0FBQSxRQUFDTCJ9
